83274d190ac28abb2942bf872395ff230bad3ab8
zzalpha-phys <zzalpha2024@163.com>
2026-03-08 21:57:49 +0800

    update.

--- paper-2026/figures/draw.jl
+++ paper-2026/figures/draw.jl
@@ -1,5 +1,6 @@
 using CairoMakie, HDF5, LinearAlgebra
-using FourierFlows, CUDA
+using FourierFlows, CUDA, Measurements
+include("../../src/postproc/circulation.jl")
 mytheme = merge(
 	Theme(
 		Axis = (
@@ -78,8 +79,10 @@ with_theme(mytheme) do
 			yscale = log10,
 			limits = (1, 512, 1e-6, 1),
 			xticks = 2 .^ (0:2:8),
+			yticks = 10.0 .^ (-6:-1),
 			xminorticks = IntervalsBetween(3),
 			yminorticks = IntervalsBetween(9),
+			ytickformat = values -> [L"10^{%$(round(Int, log10(v)))}" for v in values],
 		)
 
 		gb = GridLayout(fig[1, 2])
@@ -88,7 +91,9 @@ with_theme(mytheme) do
 			ylabel = "PDF",
 			yscale = log10,
 			limits = (-7, 7, 1e-10, 1),
-			yminorticksvisible = false,
+			yticks = 10.0 .^ (-10:2:-2),
+			yminorticks = 10.0 .^ (-10:-1),
+			ytickformat = values -> [L"10^{%$(round(Int, log10(v)))}" for v in values],
 		)
 
 		gc = GridLayout(fig[2, 1])
@@ -98,7 +103,23 @@ with_theme(mytheme) do
 			limits = (1, 512, -0.6, 0.4),
 			xticklabelsvisible = false,
 			xticks = 2 .^ (0:2:8),
+			yticks = -0.6:0.2:0.2,
 			xminorticks = IntervalsBetween(3),
+		)
+
+		axct_in = Axis(gc[1, 1];
+			height = Relative(0.4),
+			width = Relative(0.5),
+			halign = 0.1,
+			valign = 0.14,
+			xscale = log10,
+			xticklabelsvisible = false,
+			yticklabelsvisible = false,
+			limits = (1, 16, -0.01, 0.005),
+			xticks = 2 .^ (0:2:8),
+			yticks = -0.01:0.01:0.01,
+			xminorticks = IntervalsBetween(3),
+			ytickformat = values -> [L"%$(round(Int, 100*v))%" for v in values]
 		)
 		axcb = Axis(gc[2, 1];
 			xlabel = L"k",
@@ -106,6 +127,7 @@ with_theme(mytheme) do
 			xscale = log10,
 			limits = (1, 512, -0.3, 0.6),
 			xticks = 2 .^ (0:2:8),
+			yticks = -0.2:0.2:0.6,
 			xminorticks = IntervalsBetween(3),
 		)
 		rowgap!(gc, 8)
@@ -116,14 +138,16 @@ with_theme(mytheme) do
 			ylabel = L"l_\text{eq}k_f",
 			yscale = log10,
 			limits = (3.2, 4.1, 1, 10^3),
-			yminorticksvisible = false,
+			yticks = 10 .^ (0:2),
+			yminorticks = IntervalsBetween(9),
+			ytickformat = values -> [L"10^{%$(round(Int, log10(v)))}" for v in values],
 		)
 
 		for (gl, lb) in zip([ga, gb, gc, gd], ["(a)", "(b)", "(c)", "(d)"])
 			Label(gl[1, 1, TopLeft()], lb;
 				fontsize = 10,
 				font = :bold,
-				padding = (0, 22, -10, 0),
+				padding = (0, 10, -10, 0),
 			)
 		end
 
@@ -131,58 +155,90 @@ with_theme(mytheme) do
 		colgap!(fig.layout, 10)
 	end
 
-	for (Re, c) in zip(Reynolds, colormaps)
-		data = pfile["wavenumber-energy_spectra"]["$(Re)"]
-		α, β = αβs[Re]
-		x, y = read(data["x"]), read(data["y"])
-		lines!(axa, x, y; color = c)
-		if αβs[Re][1] > 0 && αβs[Re][2] > 0
-			xref = x[4:300]
-			lines!(axa, xref, eqSpec(xref, Re); linestyle = :dashdot, color = :black)
+	#data
+	begin
+		for (Re, c) in zip(Reynolds, colormaps)
+			data = pfile["wavenumber-energy_spectra"]["$(Re)"]
+			α, β = αβs[Re]
+			x, y = read(data["x"]), read(data["y"])
+			lines!(axa, x, y; color = c)
+			if αβs[Re][1] > 0 && αβs[Re][2] > 0
+				xref = x[4:300]
+				lines!(axa, xref, eqSpec(xref, Re); linestyle = :dashdot, color = :black)
+			end
 		end
-	end
-	lines!(axa, 2:64, 2e-6 .* (2:64); linestyle = :dash, color = :red)
-	lines!(axa, 2:64, 1e-1 .* (2:64) .^ (-1); linestyle = :dash, color = :blue)
-	text!(axa, 16, 1e-5; text = L"k", color = :red)
-	text!(axa, 16, 1e-2; text = L"k^{-1}", color = :blue)
 
-
-	for (Re, c) in zip(Reynolds, colormaps)
-		data = pfile["velocity-pdf:filtered"]["$(Re)"]
-		x, y, Δy = read(data["x"]), read(data["y"]), read(data["Δy"])
-		std = sqrt(sum(@. y*x^2) * (x[2]-x[1]))
-		scatter!(axb, x ./ std, y .* std; color = c)
-		errorbars!(axb, x ./ std, y .* std, Δy .* std; color = c)
-	end
-	xref = -7:0.05:7
-	yref = @. exp(-xref^2/2)/sqrt(2π)
-	lines!(axb, xref, yref; linestyle = :dashdot, color = :black)
-
-	for (Re, c) in zip(Reynolds, colormaps)
-		efdata, wfdata = pfile["wavenumber-energy_fluxes"]["$(Re)"], pfile["wavenumber-enstrophy_fluxes"]["$(Re)"]
-		x1, y1 = read(efdata["x"]), read(efdata["y"])
-		x2, y2 = read(wfdata["x"]), read(wfdata["y"])
-		lines!(axct, x1, y1; color = c)
-		lines!(axcb, x2, y2; color = c)
-	end
-
-	x = [Re for Re in Reynolds if αβs[Re][1] > 0 && αβs[Re][2] > 0]
-	y = [sqrt(αβs[Re][2]/αβs[Re][1])*1024 / 3sqrt(Re) for Re in Reynolds if αβs[Re][1] > 0 && αβs[Re][2] > 0]
-	lines!(axd, x, y; color = :black, alpha = 0.2)
-	for (Re, c) in zip(Reynolds, colormaps)
-		if αβs[Re][1] > 0 && αβs[Re][2] > 0
-			scatter!(axd, Re, sqrt(αβs[Re][2]/αβs[Re][1])*1024 / 3sqrt(Re); color = c)
+		for (Re, c) in zip(Reynolds, colormaps)
+			data = pfile["velocity-pdf:filtered"]["$(Re)"]
+			x, y, Δy = read(data["x"]), read(data["y"]), read(data["Δy"])
+			std = sqrt(sum(@. y*x^2) * (x[2]-x[1]))
+			scatter!(axb, x ./ std, y .* std;
+				strokecolor = c,
+				color = :white,
+				strokewidth = 0.5,
+				markersize = 3,
+			)
+			lines!(axb, x ./ std, y .* std;
+				color = c,
+				linewidth = 0.5,
+			)
+			errorbars!(axb, x ./ std, y .* std, Δy .* std; color = c)
 		end
+
+		for (Re, c) in zip(Reynolds, colormaps)
+			efdata, wfdata = pfile["wavenumber-energy_fluxes"]["$(Re)"], pfile["wavenumber-enstrophy_fluxes"]["$(Re)"]
+			x1, y1 = read(efdata["x"]), read(efdata["y"])
+			x2, y2 = read(wfdata["x"]), read(wfdata["y"])
+			lines!(axct, x1, y1; color = c)
+			lines!(axct_in, x1, y1; color = c)
+			lines!(axcb, x2, y2; color = c)
+		end
+
+		x = [Re for Re in Reynolds if αβs[Re][1] > 0 && αβs[Re][2] > 0]
+		y = [sqrt(αβs[Re][2]/αβs[Re][1])*1024 / 3sqrt(Re) for Re in Reynolds if αβs[Re][1] > 0 && αβs[Re][2] > 0]
+		lines!(axd, x, y; color = :black, alpha = 0.2)
+		for (Re, c) in zip(Reynolds, colormaps)
+			if αβs[Re][1] > 0 && αβs[Re][2] > 0
+				scatter!(axd, Re, sqrt(αβs[Re][2]/αβs[Re][1])*1024 / 3sqrt(Re); strokecolor = c,
+					color = :white,
+					strokewidth = 1)
+			end
+		end
+
 	end
-	Legend(gd[1, 1],
-		[LineElement(color = c, linestyle = nothing) for c in colormaps],
-		["$(Re)" for Re in Reynolds];
-		height = Relative(0.55),
-		width = Relative(0.75),
-		halign = 0,
-		valign = 1,
-		nbanks = 2,
-	)
+
+	# Decorations
+	begin
+		lines!(axa, 2:64, 2e-6 .* (2:64); linestyle = :dash, color = :red)
+		lines!(axa, 2:64, 1e-1 .* (2:64) .^ (-1); linestyle = :dash, color = :blue)
+		text!(axa, 16, 1e-5; text = L"k", color = :red)
+		text!(axa, 16, 1e-2; text = L"k^{-1}", color = :blue)
+
+		lines!(axct, [1,1.45],[-0.01,-0.11]; color = :black, linewidth = 0.5)
+		lines!(axct, [16,33],[-0.01,-0.11]; color = :black, linewidth = 0.5)
+
+
+		xref = -7:0.05:7
+		yref = @. exp(-xref^2/2)/sqrt(2π)
+		lines!(axb, xref, yref; linestyle = :dashdot, color = :black)
+
+
+		hlines!(axcb, 0; linestyle = :dashdot, linewidth = 1, color = :black)
+		hlines!(axct, 0; linestyle = :dashdot, linewidth = 1, color = :black)
+
+
+		Legend(gd[1, 1],
+			[LineElement(color = c, linestyle = nothing) for c in colormaps],
+			["$(Re)" for Re in Reynolds];
+			height = Relative(0.55),
+			width = Relative(0.75),
+			halign = 0,
+			valign = 1,
+			nbanks = 2,
+		)
+	end
+
+
 	save((@__DIR__) * "/diag.pdf", fig)
 end
 
@@ -207,8 +263,10 @@ with_theme(mytheme) do
 			ylabel = L"(⟨Γ_C^p⟩/⟨Γ_□^p⟩)^{1/p}",
 			xscale = log10,
 			xticks = 10.0 .^ (-2:0),
+			yticks = 0.9:0.05:1.1,
 			xminorticks = IntervalsBetween(9),
-			limits = (1e-2, 1, 0.98, 1.02),
+			yminorticks = IntervalsBetween(5),
+			limits = (1e-2, 1, 0.9, 1.08),
 		)
 
 		gc = GridLayout(fig[2, 1])
@@ -226,15 +284,17 @@ with_theme(mytheme) do
 			ylabel = L"(⟨Γ_C^p⟩/⟨Γ_□^p⟩)^{1/p}",
 			xscale = log10,
 			xticks = 10.0 .^ (-2:0),
+			yticks = 0.9:0.05:1.1,
 			xminorticks = IntervalsBetween(9),
-			limits = (1e-2, 1, 0.98, 1.02),
+			yminorticks = IntervalsBetween(5),
+			limits = (1e-2, 1, 0.9, 1.08),
 		)
 
 		for (gl, lb) in zip([ga, gb, gc, gd], ["(a)", "(b)", "(c)", "(d)"])
 			Label(gl[1, 1, TopLeft()], lb;
 				fontsize = 10,
 				font = :bold,
-				padding = (0, 22, -10, 0),
+				padding = (0, 10, -10, 0),
 			)
 		end
 
@@ -242,60 +302,131 @@ with_theme(mytheme) do
 		colgap!(fig.layout, 10)
 	end
 
-	for (Re, c) in zip(Reynolds, colormaps)
-		data = pfile["loop_sizes-square_moments"]["$(Re)"]
-		x_rect = read(data["2"]["5×20"]["x"])
-		y_rect = read(data["2"]["5×20"]["y"])
-		Δy_rect = read(data["2"]["5×20"]["Δy"])
-		npoints = length(x_rect)
-		x_sq = read(data["2"]["10×10"]["x"])[1:npoints]
-		y_sq = read(data["2"]["10×10"]["y"])[1:npoints]
-		Δy_sq = read(data["2"]["10×10"]["Δy"])[1:npoints]
+	# Data
+	begin
+		for (Re, c) in zip(Reynolds, colormaps)
+			data = pfile["loop_sizes-square_moments"]["$(Re)"]
+			x_rect = read(data["2"]["5×20"]["x"])
+			y_rect = read(data["2"]["5×20"]["y"])
+			Δy_rect = read(data["2"]["5×20"]["Δy"])
+			npoints = length(x_rect)
+			x_sq = read(data["2"]["10×10"]["x"])[1:npoints]
+			y_sq = read(data["2"]["10×10"]["y"])[1:npoints]
+			Δy_sq = read(data["2"]["10×10"]["Δy"])[1:npoints]
 
-		if αβs[Re][1] > 0 && αβs[Re][2] > 0
-			leq = sqrt(αβs[Re][2]/αβs[Re][1])
-			x, y = 10x_rect ./ 1024leq, sqrt.(y_rect ./ y_sq)
-			lines!(axa, x, y; color = c)
-			# scatter!(axa, x, y; color = c)
+			if αβs[Re][1] > 0 && αβs[Re][2] > 0
+				leq = sqrt(αβs[Re][2]/αβs[Re][1])
+				x, y = 10x_rect ./ 1024leq, sqrt.(y_rect ./ y_sq)
+				lines!(axa, x[1:2:(end-3)], y[1:2:(end-3)]; color = c)
+				scatter!(axa, x[1:2:(end-3)], y[1:2:(end-3)]; strokecolor = c, color = :white, strokewidth = 1)
+			end
+		end
+
+		for (Re, c) in zip(Reynolds, colormaps)
+			data = pfile["loop_sizes-square_moments"]["$(Re)"]
+			x_rect = read(data["2"]["4×16"]["x"])
+			y_rect = read(data["2"]["4×16"]["y"])
+			Δy_rect = read(data["2"]["4×16"]["Δy"])
+			npoints = length(x_rect)
+			x_sq = read(data["2"]["10×10"]["x"])[1:npoints]
+			y_sq = read(data["2"]["10×10"]["y"])[1:npoints]
+			Δy_sq = read(data["2"]["10×10"]["Δy"])[1:npoints]
+
+			if αβs[Re][1] > 0 && αβs[Re][2] > 0
+				leq = sqrt(αβs[Re][2]/αβs[Re][1])
+				x, y = 10x_rect ./ 1024leq, sqrt.(y_rect ./ y_sq)
+				lines!(axc, x[1:2:(end-3)], y[1:2:(end-3)]; color = c)
+				scatter!(axc, x[1:2:(end-3)], y[1:2:(end-3)]; strokecolor = c, color = :white, strokewidth = 1, label = "$(Re)")
+			end
+		end
+
+		markers = [:circle, :utriangle, :dtriangle, :rect, :diamond]
+		orders = 2:2:10
+		hspan!(axb, 0.99, 1.01; alpha = 0.2, color = :black)
+		for (n, mk) in zip(orders, markers)
+			data = pfile["aspect_ratio-moments"]["area8100"]["4.04"]["$(n)"]
+			x = read(data["x"])
+			y = read(data["y"])
+			Δy = read(data["Δy"])
+			scatter!(axb, x, (y ./ y[end]) .^ (1/n); marker = mk, color = :white, strokecolor = colormaps[7], strokewidth = 1, label = "$(n)")
+		end
+		hspan!(axd, 0.99, 1.01; alpha = 0.2, color = :black)
+		for (n, mk) in zip(orders, markers)
+			data = pfile["aspect_ratio-moments"]["perimeter180"]["3.5"]["$(n)"]
+			x = read(data["x"])
+			y = read(data["y"])
+			Δy = read(data["Δy"])
+			scatter!(axd, x, (y ./ y[end]) .^ (1/n); marker = mk, color = :white, strokecolor = colormaps[2], strokewidth = 1, label = "$(n)")
 		end
 	end
 
-	for (Re, c) in zip(Reynolds, colormaps)
-		data = pfile["loop_sizes-square_moments"]["$(Re)"]
-		x_rect = read(data["2"]["4×16"]["x"])
-		y_rect = read(data["2"]["4×16"]["y"])
-		Δy_rect = read(data["2"]["4×16"]["Δy"])
-		npoints = length(x_rect)
-		x_sq = read(data["2"]["10×10"]["x"])[1:npoints]
-		y_sq = read(data["2"]["10×10"]["y"])[1:npoints]
-		Δy_sq = read(data["2"]["10×10"]["Δy"])[1:npoints]
+	# Decorations
+	begin
+		hlines!(axa, 1; linestyle = :dashdot, linewidth = 1, color = :black)
+		text!(axa, 0.05, 0.95; text = L"A_C = A_\square", space = :relative, align = (:left, :top))
 
-		if αβs[Re][1] > 0 && αβs[Re][2] > 0
-			leq = sqrt(αβs[Re][2]/αβs[Re][1])
-			x, y = 10x_rect ./ 1024leq, sqrt.(y_rect ./ y_sq)
-			lines!(axc, x, y; color = c)
-			# scatter!(axc, x, y; color = c)
+		hlines!(axc, 1; linestyle = :dashdot, linewidth = 1, color = :black)
+		axislegend(axc; orientation = :vertical, colgap = 2, patchlabelgap = 0, valign = 0, halign = 1, padding = 1, nbanks = 2)
+		text!(axc, 0.05, 0.95; text = L"L_C = L_\square", space = :relative, align = (:left, :top))
+
+		axislegend(axb; orientation = :horizontal, colgap = 2, patchlabelgap = 0, valign = 0, halign = 0, padding = 0, nbanks = 2)
+		leq = sqrt(αβs[4.04][2]/αβs[4.04][1])
+		text!(axb, 0.5, 0.85; text = L"{A_C} = %$(round((90/1024/leq)^2; digits = 5))A_\text{eq}",
+			space = :relative,
+			align = (:center, :bottom),
+		)
+		text!(axb, 0.07, 0.25; text = L"Re = 4.04",
+			space = :relative,
+			align = (:left, :bottom),
+		)
+		text!(axb, 0.012, 1; text = L"\pm 1%",
+			align = (:left, :center),
+		)
+
+
+		axislegend(axd; orientation = :horizontal, colgap = 2, patchlabelgap = 0, valign = 0, halign = 0, padding = 0, nbanks = 2)
+		leq = sqrt(αβs[3.5][2]/αβs[3.5][1])
+		text!(axd, 0.5, 0.85; text = L"L_C = %$(round((90/1024/leq); digits = 3))L_\text{eq}", space = :relative, align = (:center, :bottom))
+		text!(axd, 0.07, 0.25; text = L"Re = 3.5",
+			space = :relative,
+			align = (:left, :bottom),
+		)
+		text!(axd, 0.012, 1; text = L"\pm 1%",
+			align = (:left, :center),
+		)
+		begin
+			grid = TwoDGrid(GPU(); nx = 1024, Lx = 2π)
+			loopsizes = [15; 20; 30; 40; 45; 60; 75; 90]
+			rects = [rectsloop(grid, l, 8100 ÷ l, 1, 1) for l in loopsizes]
+			aspect_ratios = loopsizes .^ 2 ./ 8100
+			ζh_IR = grid.Krsq .^ (-1/3)
+			CUDA.@allowscalar ζh_IR[1, 1] = 0
+			vars = map(rects) do hsh
+				Γ = getΓ(ζh_IR, hsh, grid)
+				sum(x->x^2, Γ)
+			end
+			vars_ratio = vars ./ vars[end]
+			@show vars_ratio
+			lines!(axb, aspect_ratios, sqrt.(vars_ratio); linestyle = :dash, color = :purple)
 		end
-	end
-	markers = [:circle, :utriangle, :dtriangle, :rect, :diamond]
-	orders = 2:2:10
 
-	for (n, mk) in zip(orders, markers)
-		data = pfile["aspect_ratio-moments"]["area8100"]["4.04"]["$(n)"]
-		x = read(data["x"])
-		y = read(data["y"])
-		Δy = read(data["Δy"])
-		scatter!(axb, x, (y ./ y[end]) .^ (1/n); marker = mk, color = :transparent, strokecolor = :black, strokewidth = 0.5)
-	end
+		begin
+			grid = TwoDGrid(GPU(); nx = 1024, Lx = 2π)
+			loopsizes = 10:10:90
+			rects = [rectsloop(grid, l, 180-l, 1, 1) for l in loopsizes]
+			aspect_ratios = loopsizes .^ 2 ./ 8100
+			ζh_IR = grid.Krsq .^ (-1/3)
+			CUDA.@allowscalar ζh_IR[1, 1] = 0
+			vars = map(rects) do hsh
+				Γ = getΓ(ζh_IR, hsh, grid)
+				sum(x->x^2, Γ)
+			end
+			vars_ratio = vars ./ vars[end]
+			@show vars_ratio
+			lines!(axd, aspect_ratios, sqrt.(vars_ratio); linestyle = :dash, color = :purple)
+		end
 
-	for (n, mk) in zip(orders, markers)
-		data = pfile["aspect_ratio-moments"]["perimeter180"]["3.5"]["$(n)"]
-		x = read(data["x"])
-		y = read(data["y"])
-		Δy = read(data["Δy"])
-		scatter!(axd, x, (y ./ y[end]) .^ (1/n); marker = mk, color = :transparent, strokecolor = :black, strokewidth = 0.5)
 	end
-
 
 	save((@__DIR__) * "/areaperi.pdf", fig)
 end
@@ -322,9 +453,11 @@ with_theme(mytheme) do
 			ylabel = L"\alpha^{p/2} ⟨|Γ_C|^p⟩",
 			xscale = log10,
 			yscale = log10,
-			xticks = 10.0 .^ (-2:0),
+			xticks = 10.0 .^ (-2:2),
 			xminorticks = IntervalsBetween(9),
-			limits = (1e-2, 1, 0.98, 1.02),
+			yminorticks = 10.0 .^ (0:30),
+			limits = (5e-3, 1e2, 1e-2, 1e30),
+			xtickformat = values -> [L"10^{%$(round(Int, log10(v)))}" for v in values],
 		)
 
 		for (gl, lb) in zip([ga, gb], ["(a)", "(b)"])
@@ -345,18 +478,21 @@ with_theme(mytheme) do
 			α = αβs[Re][1]
 			for n in 1:10
 				data = pfile["loop_sizes-square_moments"]["$(Re)"]["$(n)"]["10×10"]
-				x, y = read(data["x"]), read(data["y"])
+				x, y = read(data["x"])[1:2:(end-64)], read(data["y"])[1:2:(end-64)]
 				lines!(axb, 10x ./ 1024leq, y .* α ^ (n/2); color = c)
-				scatter!(axb, 10x ./ 1024leq, y .* α ^ (n/2); color = :white, strokecolor = c, strokewidth = 0.3)
+				scatter!(axb, 10x ./ 1024leq, y .* α ^ (n/2); color = :white, strokecolor = c, strokewidth = 0.5, markersize = 3, label = "$(Re)")
 			end
 		end
 	end
+
+	axislegend(axb; unique = true, orientation = :vertical, colgap = 0, patchlabelgap = 0, valign = 1, halign = 0, padding = 0, nbanks = 2)
 
 	sc_area = map(1:10) do n
 		data = pfile["loop_sizes-square_moments"]["4.04"]["$(n)"]["10×10"]
 		leq = sqrt(αβs[4.04][2]/αβs[4.04][1])
 		α = αβs[4.04][1]
-		x, y = read(data["x"])[2:16], read(data["y"])[2:16]
+		x, y, Δy = read(data["x"])[1:12], read(data["y"])[1:12], read(data["Δy"])[1:12]
+		y = y .± Δy
 		fit = hcat(log.(x), ones(length(x)))\log.(y)
 		yref = exp.(hcat(log.(x), ones(length(x))) * fit)
 		lines!(axb, 10x ./ 1024leq, yref .* α ^ (n/2); color = :blue)
@@ -367,16 +503,21 @@ with_theme(mytheme) do
 		data = pfile["loop_sizes-square_moments"]["3.5"]["$(n)"]["10×10"]
 		leq = sqrt(αβs[3.5][2]/αβs[3.5][1])
 		α = αβs[3.5][1]
-		x, y = read(data["x"])[2:100], read(data["y"])[2:100]
+		x, y = read(data["x"])[2:64], read(data["y"])[2:64]
 		fit = hcat(log.(x), ones(length(x)))\log.(y)
 		yref = exp.(hcat(log.(x), ones(length(x))) * fit)
 		lines!(axb, 10x ./ 1024leq, yref .* α ^ (n/2); color = :red)
 		fit[1]
 	end
-	scatter!(axa, 1:10,sc_area,color = :blue)
-	scatter!(axa, 1:10,sc_peri,color = :red)
-	autolimits!(axb)
-
+	scatter!(axa, 1:10, sc_area, strokecolor = :blue, color = :transparent, strokewidth = 1, label = L"l\ll 2\pi l_\text{eq}")
+	scatter!(axa, 1:10, sc_peri, strokecolor = :red, color = :transparent, strokewidth = 1, label = L"l\gg 2\pi l_\text{eq}")
+	errorbars!(axa, 1:10, Measurements.value.(sc_area), 3 .* Measurements.uncertainty.(sc_area), color = :blue)
+	errorbars!(axa, 1:10, Measurements.value.(sc_peri), 3 .* Measurements.uncertainty.(sc_peri), color = :red)
+	lines!(axa, 0:11, 0:11; color = :blue, linestyle = :dash)
+	lines!(axa, 0:11, 0.5 .* (0:11); color = :red, linestyle = :dash)
+	text!(axa, 6.5, 3; text = L"p/2", color = :red, align = (:left, :top))
+	text!(axa, 5.5, 6; text = L"p", color = :blue, align = (:right, :bottom))
+	axislegend(axa; halign = 0, valign = 1, padding = 0)
 
 	save((@__DIR__) * "/scaling.pdf", fig)
 end
